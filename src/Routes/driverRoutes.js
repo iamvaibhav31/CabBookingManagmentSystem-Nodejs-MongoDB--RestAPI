044a2f7289d7d8a1aa570dbcd3eb3b231f667cc0
vaibhav  sharma <80534369+iamvaibhav31@users.noreply.github.com>
2022-10-22 21:47:40 +0530

    Update driverRoutes.js

--- src/Routes/driverRoutes.js
+++ src/Routes/driverRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router()
 
 
 router.post('/register', drivercontroller.register)
+router.post('/isBooking', drivercontroller.isbooking)
+router.post('/acceptBooking', drivercontroller.acceptbooking)
 router.post('/drivehistory', drivercontroller.drivehistory)
 router.post('/endride', drivercontroller.enddrive)
-// router.post('/resentbooking', drivercontroller.register)
 module.exports = router;
 
